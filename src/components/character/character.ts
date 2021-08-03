7a37d9b47464d99d041661ffe0a1879aff67b233
phlgr <philipp@gartz.dev>
2021-08-03 17:09:47 +0200

    ✨ Create character card component

--- src/components/character/character.ts
+++ src/components/character/character.ts
@@ -2,5 +2,27 @@ import './character.css';
 import { createElement } from '../../utils/createElement';
 
 export function createCharacterCard(): HTMLElement {
-  return createElement('div', { className: 'card' });
+  return createElement('div', {
+    className: 'character-card',
+    childElements: [
+      createElement('img', {
+        className: 'character-card__image',
+        src: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+      }),
+      createElement('article', {
+        className: 'character-card__info',
+        childElements: [
+          createElement('h2', { innerText: 'Rick Sanchez' }),
+          createElement('div', {
+            childElements: [
+              createElement('h5', { innerText: 'Status:' }),
+              createElement('p', { innerText: '💚 Alive - Human' }),
+            ],
+          }),
+          createElement('h5', { innerText: 'Origin:' }),
+          createElement('p', { innerText: 'Earth (C-137)' }),
+        ],
+      }),
+    ],
+  });
 }
