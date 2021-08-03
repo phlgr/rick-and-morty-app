acab25eaf93672c7fd191d02a304a08d1b0634d8
phlgr <philipp@gartz.dev>
2021-08-03 14:14:33 +0200

    ✨ Create main element and searchbar

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import './style.css';
 
-const app = document.querySelector<HTMLDivElement>('#app');
-
 const mainElement = document.createElement('main');
 
 const title = document.createElement('h1');
@@ -11,6 +9,8 @@ const input = document.createElement('input');
 input.placeholder = 'Search for a character...';
 
 mainElement.append(title, input);
+
+const app = document.querySelector<HTMLDivElement>('#app');
 
 if (app !== null) {
   app.append(mainElement);
