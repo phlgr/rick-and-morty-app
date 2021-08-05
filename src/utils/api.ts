7364b50206320a64da83127e21c4d40810ce41da
phlgr <philipp@gartz.dev>
2021-08-05 17:04:18 +0200

    ✨ Add filter to getCharacters

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,9 @@
 import type { AllCharactersFromAPI, Character } from '../types';
 
-export async function getCharacters(): Promise<Character[]> {
-  const response = await fetch('https://rickandmortyapi.com/api/character');
+export async function getCharacters(name?: string): Promise<Character[]> {
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character/?name=${name ? name : ''}`
+  );
   const data: AllCharactersFromAPI = await response.json();
   const characters = data.results;
   const formattedCharacters: Character[] = characters.map((character) => {
