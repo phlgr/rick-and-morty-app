fa70298dc099903dd4eda1be9c437b3f7180c257
phlgr <philipp@gartz.dev>
2021-08-05 15:27:43 +0200

    🏷️ Create Types for Character Results from API

--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,34 @@ export type Character = {
   species: string;
   origin: string;
 };
+
+export type CharacterFromAPI = {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: {
+    name: string;
+    url: string;
+  };
+  location: {
+    name: string;
+    url: string;
+  };
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+};
+
+export type AllCharactersFromAPI = {
+  info: {
+    count: number;
+    pages: number;
+    next: string | null;
+    prev: string | null;
+  };
+  results: CharacterFromAPI[];
+};
