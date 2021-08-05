e2deee51dee16fb552784d1fa2c025d9475baae5
phlgr <philipp@gartz.dev>
2021-08-05 15:28:11 +0200

    ✨ Transform data to match our Type

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,19 @@
-export async function getCharacters() {
+import type { AllCharactersFromAPI, Character } from '../types';
+
+export async function getCharacters(): Promise<Character[]> {
   const response = await fetch('https://rickandmortyapi.com/api/character');
-  const data = await response.json();
+  const data: AllCharactersFromAPI = await response.json();
   const characters = data.results;
-  return characters;
+  const formattedCharacters: Character[] = characters.map((character) => {
+    const formattedCharacter: Character = {
+      name: character.name,
+      origin: character.origin.name,
+      species: character.species,
+      status: character.status,
+      thumbnail: character.image,
+    };
+    return formattedCharacter;
+  });
+
+  return formattedCharacters;
 }
