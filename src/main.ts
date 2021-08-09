244b91fb6486d31776257d44bd7ed3d745883305
phlgr <philipp@gartz.dev>
2021-08-09 17:05:32 +0200

    🚑 Fix top level await

--- src/main.ts
+++ src/main.ts
@@ -4,36 +4,42 @@ import type { Character } from './types';
 import { getCharacters } from './utils/api';
 import { createElement } from './utils/createElement';
 
-const characters: Character[] = await getCharacters();
+async function run() {
+  const characters: Character[] = await getCharacters();
 
-const characterContainer = createElement('div', {
-  className: 'characterContainer',
-  childElements: characters.map((character) => createCharacterCard(character)),
-});
+  const characterContainer = createElement('div', {
+    className: 'characterContainer',
+    childElements: characters.map((character) =>
+      createCharacterCard(character)
+    ),
+  });
 
-const searchbar = createElement('input', {
-  placeholder: 'Search for a character',
-  oninput: async () => {
-    characterContainer.innerHTML = '';
+  const searchbar = createElement('input', {
+    placeholder: 'Search for a character',
+    oninput: async () => {
+      characterContainer.innerHTML = '';
 
-    const search = searchbar.value;
+      const search = searchbar.value;
 
-    const filteredCharacters = await getCharacters(search);
+      const filteredCharacters = await getCharacters(search);
 
-    const filteredCharacterElements = filteredCharacters.map(
-      (filteredCharacter) => createCharacterCard(filteredCharacter)
-    );
+      const filteredCharacterElements = filteredCharacters.map(
+        (filteredCharacter) => createCharacterCard(filteredCharacter)
+      );
 
-    characterContainer.append(...filteredCharacterElements);
-  },
-});
+      characterContainer.append(...filteredCharacterElements);
+    },
+  });
 
-const mainElement = createElement('main', {
-  childElements: [
-    createElement('h1', { innerText: 'Rick and Morty' }),
-    searchbar,
-    characterContainer,
-  ],
-});
+  const mainElement = createElement('main', {
+    childElements: [
+      createElement('h1', { innerText: 'Rick and Morty' }),
+      searchbar,
+      characterContainer,
+    ],
+  });
 
-document.querySelector<HTMLDivElement>('#app')?.append(mainElement);
+  document.querySelector<HTMLDivElement>('#app')?.append(mainElement);
+}
+
+run();
